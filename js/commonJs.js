45008d0f55194fc30dff14d2778bf05d00febe1d
YMJxiaojia <YMJxiaojia@users.noreply.github.com>
2015-12-01 09:47:33 +0800

    Delete commonJs.js

--- js/commonJs.js
+++
@@ -1,6 +0,0 @@
-var iScale = 1;
-iScale = iScale / window.devicePixelRatio;
-document.write('<meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=' + iScale + ',minimum-scale=' + iScale + ',maximum-scale=' + iScale + '">')
-var iWidth = document.documentElement.clientWidth;
-iWidth = iWidth > 1620 ? 1620 : iWidth;
-document.getElementsByTagName('html')[0].style.fontSize = iWidth / 64*10 + 'px';
